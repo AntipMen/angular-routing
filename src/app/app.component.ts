98c44cb9b56ec1c13f0c11e3d122929eed0436a0
Anna <Anna.Antipova794gmail.com>
2021-03-13 22:04:05 +0200

    add practice lesson with angular routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-practice';
+  constructor(public auth: AuthService) {
+  }
 }
